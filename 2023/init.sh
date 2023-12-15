64a815b2326e9de6f02b91be6453cf89474a0a7c
LaJase <jason.richier@gmail.com>
2023-12-15 15:00:36 +0100

    chore(init): update init script with inputs

--- 2023/init.sh
+++ 2023/init.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 1 ]; then
 	echo
-	echo "Il faut entrer le numéro du jour"
+	echo "Enter day's number please"
 	echo
 	echo "  $0 <day_num>"
 	echo "      day_num: num of the day (ex: 10)"
@@ -16,3 +16,11 @@ folder=$(dirname "${0}")
 CMD="cp ${folder}/code-starter.py ${folder}/code.day${DAY}.py"
 echo "${CMD}"
 eval "${CMD}"
+
+CMD_INPUT="touch $folder/puzzles/input.day$DAY.txt"
+echo "${CMD_INPUT}"
+eval "${CMD_INPUT}"
+
+CMD_INPUT_START="touch $folder/puzzles/input.day$DAY.start.txt"
+echo "${CMD_INPUT_START}"
+eval "${CMD_INPUT_START}"
