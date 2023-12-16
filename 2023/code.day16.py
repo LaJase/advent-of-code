0bd000ec0baed5c58fab1cbe8ea1b5d32e4c0b73
LaJase <jason.richier@gmail.com>
2023-12-16 07:17:42 +0100

    feat(day): wip day16

---
+++ 2023/code.day16.py
@@ -0,0 +1,109 @@
+#! /usr/bin/python3.10
+
+import sys
+import copy
+import time
+
+print()
+print("=========================================")
+with open(sys.argv[1], "r") as file:
+    file_content = file.read().strip()
+    lines = file_content.split("\n")
+    records = [[c for c in row] for row in lines]
+
+line_number = len(records)
+col_number = len(records[0])
+
+assoc = {
+    "|": {
+        "right": {"move": [(0, 1), (0, -1)], "dest": "north"},
+        "left": {"move": (0, -1), "dest": "south"},
+    },
+    "-": {
+        "east": {"move": (-1, 0), "dest": "east"},
+        "west": {"move": (1, 0), "dest": "west"},
+    },
+    "/": {
+        "north": {"move": (1, 0), "dest": "west"},
+        "east": {"move": (0, -1), "dest": "south"},
+    },
+    "\\": {
+        "north": {"move": (-1, 0), "dest": "east"},
+        "west": {"move": (0, -1), "dest": "south"},
+    },
+}
+
+
+def follower(grid, pos, grid_length):
+    for x, y in pos:
+        grid[y][x] = "#"
+
+    for i in range(grid_length):
+        print("".join(grid[i]))
+
+
+summerized = copy.deepcopy(records)
+
+tiles = []
+tiles.append((0, 0, "right"))
+energized = set()
+while len(tiles) > 0:
+    # print(tiles)
+    x, y, direction = tiles.pop()
+    if 0 <= x < col_number and 0 <= y < line_number:
+        energized.add((x, y))
+        print()
+        print(x, y, direction, records[y][x])
+        print(follower(summerized, energized, line_number))
+        # print(energized)
+        # print(x, y, direction)
+
+        if records[y][x] == "|":
+            if direction == "right" or direction == "left":
+                tiles.append((x, y + 1, "down"))
+                tiles.append((x, y - 1, "up"))
+            else:
+                tiles.append((x, y, direction))
+        if records[y][x] == "-":
+            if direction == "down" or direction == "up":
+                tiles.append((x - 1, y, "left"))
+                tiles.append((x + 1, y, "right"))
+            elif direction == "right":
+                tiles.append((x, y + 1, "right"))
+            elif direction == "left":
+                tiles.append((x, y - 1, "left"))
+        if records[y][x] == "/":
+            if direction == "down":
+                tiles.append((x - 1, y, "left"))
+            elif direction == "up":
+                tiles.append((x + 1, y, "right"))
+            elif direction == "right":
+                tiles.append((x, y - 1, "up"))
+            elif direction == "up":
+                tiles.append((x + 1, y, "down"))
+        if records[y][x] == "\\":
+            if direction == "down":
+                tiles.append((x + 1, y, "right"))
+            elif direction == "up":
+                tiles.append((x - 1, y, "left"))
+            elif direction == "right":
+                tiles.append((x + 1, y, "down"))
+            elif direction == "left":
+                tiles.append((x, y - 1, "up"))
+        if records[y][x] == ".":
+            if direction == "down":
+                tiles.append((x, y + 1, "down"))
+            elif direction == "up":
+                tiles.append((x, y - 1, "up"))
+            elif direction == "right":
+                tiles.append((x + 1, y, "right"))
+            elif direction == "left":
+                tiles.append((x - 1, y, "left"))
+
+    # time.sleep(1)
+
+
+print(energized)
+print("first star :", len(energized))
+
+print("second star:")
