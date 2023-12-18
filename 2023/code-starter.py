dee67ab73d49cd3d1f91028f27f7b6a6c8ca0d31
LaJase <jason.richier@gmail.com>
2023-12-18 16:15:46 +0100

    chore(init): update python template

--- 2023/code-starter.py
+++ 2023/code-starter.py
@@ -2,13 +2,26 @@
 
 import sys
 
+from time import perf_counter
+
+total_start_time = perf_counter()
 print()
 print("=========================================")
+
 with open(sys.argv[1], "r") as file:
     file_content = file.read().strip()
     lines = file_content.split("\n")
     records = [[c for c in row] for row in lines]
 
-print("first star :")
 
+start_time = perf_counter()
+print("first star :")
+print("Time:", round(perf_counter() - start_time, 4), "sec")
+print()
+
+start_time = perf_counter()
 print("second star:")
+print("Time:", round(perf_counter() - start_time, 4), "sec")
+print()
+print("Total time:", round(perf_counter() - total_start_time, 4), "sec")
+print("=========================================")
