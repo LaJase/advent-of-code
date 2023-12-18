e42659a7787416cbe2d3cc41d045b28d8a005d01
LaJase <jason.richier@gmail.com>
2023-12-18 17:57:38 +0100

    chore(init): download input auto

--- 2023/init.sh
+++ 2023/init.sh
@@ -11,16 +11,18 @@ fi
 
 DAY=$1
 
-# On copie le script
 folder=$(dirname "${0}")
+SESSION_TOKEN=$(cat "$folder"/session-token.txt)
+
+# On copie le script
 CMD="cp ${folder}/code-starter.py ${folder}/code.day${DAY}.py"
 echo "${CMD}"
 eval "${CMD}"
 
-CMD_INPUT="touch $folder/puzzles/input.day$DAY.txt"
-echo "${CMD_INPUT}"
-eval "${CMD_INPUT}"
-
 CMD_INPUT_START="touch $folder/puzzles/input.day$DAY.start.txt"
 echo "${CMD_INPUT_START}"
 eval "${CMD_INPUT_START}"
+
+CMD_INPUT="curl 'https://adventofcode.com/2023/day/$DAY/input' -H 'cookie: session=$SESSION_TOKEN' --compressed -o ./$folder/puzzles/input.day$DAY.txt"
+echo "${CMD_INPUT}"
+eval "${CMD_INPUT}"
